6c72d26c312ccb626f33a198b5c97b96ee1910b6
Joaquín Antonio Galdame <92065354+JoaquinGaldame@users.noreply.github.com>
2025-08-09 03:10:47 -0300

    Add Charts.js Reports

--- src/app/features/inventory/inventory.ts
+++ src/app/features/inventory/inventory.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 //Components
 import { List } from './list/list';
+import { Reports } from './reports/reports';
 import { TabsModule } from 'primeng/tabs';
 
 @Component({
   selector: 'app-inventory',
-  imports: [List, TabsModule],
+  imports: [List, Reports, TabsModule],
   templateUrl: './inventory.html',
   styleUrl: './inventory.css'
 })
--- src/app/features/inventory/reports/reports.ts
+++ src/app/features/inventory/reports/reports.ts
@@ -1,11 +1,118 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, effect, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { ChartModule } from 'primeng/chart';
+import { ButtonModule } from 'primeng/button';
+import { MessageService } from 'primeng/api';
+import { isPlatformBrowser } from '@angular/common';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-reports',
-  imports: [],
+  imports: [ChartModule, ButtonModule],
+  providers:[MessageService],
   templateUrl: './reports.html',
   styleUrl: './reports.css'
 })
-export class Reports {
+export class Reports implements OnInit {
+  basicData: any;
 
+  basicOptions: any;
+  
+  items: MenuItem[] | undefined;
+  platformId = inject(PLATFORM_ID);
+
+
+  constructor(
+    private cd: ChangeDetectorRef,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit() {
+            this.items = [
+            {
+                icon: 'pi pi-pencil',
+                command: () => {
+                    this.messageService.add({ severity: 'info', summary: 'Add', detail: 'Data Added' });
+                }
+            },
+            {
+                icon: 'pi pi-refresh',
+                command: () => {
+                    this.messageService.add({ severity: 'success', summary: 'Update', detail: 'Data Updated' });
+                }
+            },
+            {
+                icon: 'pi pi-trash',
+                command: () => {
+                    this.messageService.add({ severity: 'error', summary: 'Delete', detail: 'Data Deleted' });
+                }
+            },
+            {
+                icon: 'pi pi-upload',
+                routerLink: ['/fileupload']
+            },
+            {
+                icon: 'pi pi-external-link',
+                target:'_blank',
+                url: 'https://angular.dev'
+            }
+        ];
+    this.initChart();
+  }
+
+  initChart() {
+    if (isPlatformBrowser(this.platformId)) {
+        const documentStyle = getComputedStyle(document.documentElement);
+        const textColor = documentStyle.getPropertyValue('--p-text-color');
+        const textColorSecondary = documentStyle.getPropertyValue('--p-text-muted-color');
+        const surfaceBorder = documentStyle.getPropertyValue('--p-content-border-color');
+
+        this.basicData = {
+            labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+            datasets: [
+                {
+                    label: 'Sales',
+                    data: [540, 325, 702, 620],
+                    backgroundColor: [
+                        'rgba(249, 115, 22, 0.2)',
+                        'rgba(6, 182, 212, 0.2)',
+                        'rgb(107, 114, 128, 0.2)',
+                        'rgba(139, 92, 246, 0.2)',
+                    ],
+                    borderColor: ['rgb(249, 115, 22)', 'rgb(6, 182, 212)', 'rgb(107, 114, 128)', 'rgb(139, 92, 246)'],
+                    borderWidth: 1,
+                },
+            ],
+        };
+
+        this.basicOptions = {
+            plugins: {
+                legend: {
+                    labels: {
+                        color: textColor,
+                    },
+                },
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: textColorSecondary,
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                    },
+                },
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        color: textColorSecondary,
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                    },
+                },
+            },
+        };
+        this.cd.markForCheck()
+    }
+  }
 }
